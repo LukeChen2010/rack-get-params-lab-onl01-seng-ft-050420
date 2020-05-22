320f67fa3eb7c86c1e2d1725eaf429a9340bd007
Luke Chen <luke.chen.2010@gmail.com>
2020-05-22 15:24:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -34,7 +34,7 @@ class Application
       
       if @@items.include?(search_term)
         @@cart << search_term
-        esp.write "added #{search_term}"
+        resp.write "added #{search_term}"
       else
         resp.write "We don't have that item"
       end
