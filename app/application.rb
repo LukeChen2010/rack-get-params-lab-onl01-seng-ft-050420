e29bb3da0a71ed880ddd589fb2f5770ae8ee2cc1
Luke Chen <luke.chen.2010@gmail.com>
2020-05-22 15:31:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -36,7 +36,7 @@ class Application
         @@cart << search_term
         resp.write "added #{search_term}"
       else
-        resp.write "We don't have that item"
+        resp.write "We don't have that item #{search_term}"
       end
       
     else
