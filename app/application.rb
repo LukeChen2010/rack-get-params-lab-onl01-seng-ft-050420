9287e3e756fbd939fdba0d938d47b4d0df57602e
Luke Chen <luke.chen.2010@gmail.com>
2020-05-22 15:27:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -31,7 +31,7 @@ class Application
     elsif req.path.match(/add/)
       
       search_term = req.params["q"]
-      
+      resp.write "#{search_term}"
       if @@items.include?(search_term)
         @@cart << search_term
         resp.write "added #{search_term}"
