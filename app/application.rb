b68310d3d7f75663039ba87e2a0ec7304b8f2f59
Luke Chen <luke.chen.2010@gmail.com>
2020-05-22 15:11:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -21,6 +21,10 @@ class Application
     elsif req.path.match(/add/)
       search_term = req.params["q"]
       if @@cart.include?(search_term)
+        @@cart << search_term
+      else
+        
+      end
     else
       resp.write "Path Not Found"
     end
