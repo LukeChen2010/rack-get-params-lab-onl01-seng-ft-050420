c7ba9cf6bec8608d3c04ccef9810e8bf3fd5cb5b
Luke Chen <luke.chen.2010@gmail.com>
2020-05-22 15:27:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -32,12 +32,13 @@ class Application
       
       search_term = req.params["q"]
       resp.write "#{search_term}"
-      if @@items.include?(search_term)
-        @@cart << search_term
-        resp.write "added #{search_term}"
-      else
-        resp.write "We don't have that item"
-      end
+      
+      #if @@items.include?(search_term)
+      #  @@cart << search_term
+      #  resp.write "added #{search_term}"
+      #else
+      #  resp.write "We don't have that item"
+      #end
       
     else
       
