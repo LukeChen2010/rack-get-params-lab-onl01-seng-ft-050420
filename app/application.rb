197e53b4a23c9fd50b3d25b452e481a0c9b93796
Luke Chen <luke.chen.2010@gmail.com>
2020-05-22 15:30:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -31,14 +31,13 @@ class Application
     elsif req.path.match(/add/)
       
       search_term = req.params["q"]
-      resp.write "#{search_term}"
-      
-      #if @@items.include?(search_term)
-      #  @@cart << search_term
-      #  resp.write "added #{search_term}"
-      #else
-      #  resp.write "We don't have that item"
-      #end
+
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item"
+      end
       
     else
       
